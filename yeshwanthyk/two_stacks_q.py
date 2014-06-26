00642abad559f3aea6ce45849400ace4f1b4683a
yeshwanthyk <yeshwanth.yk@gmail.com>
2014-06-26 00:44:07 -0400

    Add main block

--- yeshwanthyk/two_stacks_q.py
+++ yeshwanthyk/two_stacks_q.py
@@ -16,3 +16,14 @@ class Queue:
             print self.stack2.pop()
         except IndexError:
             print "No more elements in the queue"
+def main():
+    q = Queue()
+    q.enq(1)
+    q.enq(2)
+
+    q.deq()
+    q.deq()
+    q.deq()
+
+if __name__ == "__main__":
+    main()
