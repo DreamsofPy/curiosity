423e77e5d6a11c6359d6d77f68f1489cadb4903e
yeshwanthyk <yeshwanth.yk@gmail.com>
2014-06-26 00:44:36 -0400

    Add docs for two_stacks_q [yesh]

--- yeshwanthyk/two_stacks_q.py
+++ yeshwanthyk/two_stacks_q.py
@@ -1,17 +1,20 @@
 """Using two stacks recreate a queue"""
 class Queue:
+    # Initialize two empty stacks
     def __init__(self):
         self.stack1 = []
         self.stack2 = []
 
+    # On enqueue we keep adding the values to stack 1
     def enq(self, x):
         self.stack1.append(x)
 
+    # On dequeue we check to see if it empty, if not we pop the element for
+    # stack2. Else, we pop all elements for stack1 and append them into stack2
     def deq(self):
         if not self.stack2:
             for i in xrange(len(self.stack1)):
                 self.stack2.append(self.stack1.pop())
-        print self.stack2.pop()
         try:
             print self.stack2.pop()
         except IndexError:
